ba8f3af4bb432b5af4220e2eb2bcd6d582499e58
VIKINGYFY <viking.yfy@qq.com>
2025-02-26 17:20:38 +0800

    Update QCA Source

--- target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
+++ target/linux/qualcommax/ipq50xx/base-files/lib/upgrade/platform.sh
@@ -72,6 +72,11 @@ platform_check_image() {
 platform_do_upgrade() {
 	case "$(board_name)" in
 	linksys,mr5500|\
+	jdcloud,re-cs-03)
+		CI_KERNPART="0:HLOS"
+		CI_ROOTPART="rootfs"
+		emmc_do_upgrade "$1"
+		;;
 	linksys,mx2000|\
 	linksys,mx5500|\
 	linksys,spnmx56)
@@ -81,6 +86,14 @@ platform_do_upgrade() {
 		;;
 	*)
 		default_do_upgrade "$1"
+		;;
+	esac
+}
+
+platform_copy_config() {
+	case "$(board_name)" in
+	jdcloud,re-cs-03)
+		emmc_copy_config
 		;;
 	esac
 }
