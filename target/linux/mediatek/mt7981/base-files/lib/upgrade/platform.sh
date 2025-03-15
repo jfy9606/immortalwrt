7180de7e4d3c88f8ef9963493a7069252b5c1c66
jfy <jfy9606@gmail.com>
2025-03-15 04:58:36 +0000

    update xr30 support

--- target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/mt7981/base-files/lib/upgrade/platform.sh
@@ -203,6 +203,7 @@ platform_do_upgrade() {
 	nradio,wt9103 |\
 	cmcc,a10 |\
 	cmcc,rax3000m |\
+	cmcc,xr30 |\
 	h3c,nx30pro |\
 	*konka,komi-a31* |\
 	*nokia,ea0326gmp* |\
@@ -210,6 +211,7 @@ platform_do_upgrade() {
 		nand_do_upgrade "$1"
 		;;
 	cmcc,rax3000m-emmc |\
+	cmcc,xr30-emmc |\
 	*emmc*)
 		CI_KERNPART="kernel"
 		CI_ROOTPART="rootfs"
@@ -250,6 +252,7 @@ platform_check_image() {
 	*jcg,q30* |\
 	cmcc,a10 |\
 	cmcc,rax3000m* |\
+	cmcc,xr30* |\
 	h3c,nx30pro |\
 	*konka,komi-a31* |\
 	nradio,wt9103 |\
