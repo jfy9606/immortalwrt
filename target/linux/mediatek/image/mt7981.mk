2067acb6846b4b111d05678fe7b6108c03e15eb4
jfy <jfy9606@gmail.com>
2025-03-21 01:42:47 +0000

    fix mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -494,14 +494,6 @@ define Device/cmcc_rax3000m-usboffload
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   DEVICE_PACKAGES := $(MT7981_USB_PKGS) swconfig luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
   SUPPORTED_DEVICES := cmcc,rax3000m-usboffload
-
-define Device/cmcc_xr30
-  DEVICE_VENDOR := CMCC
-  DEVICE_MODEL := XR30 NAND
-  DEVICE_DTS := mt7981-cmcc-xr30
-  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4
-  SUPPORTED_DEVICES := cmcc,xr30
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -526,6 +518,25 @@ define Device/cmcc_rax3000m-emmc-usboffload
 endef
 TARGET_DEVICES += cmcc_rax3000m-emmc-usboffload
 
+define Device/cmcc_xr30
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := XR30 NAND
+  DEVICE_DTS := mt7981-cmcc-xr30
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4
+  SUPPORTED_DEVICES := cmcc,xr30
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4
+  SUPPORTED_DEVICES := cmcc,xr30
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 116736k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
 TARGET_DEVICES += cmcc_xr30
 
 define Device/cmcc_xr30-emmc
