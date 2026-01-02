dcd9cef7221ef49459994e3509ed608b23571545
jfy <jfy9606@qq.com>
2026-01-02 07:05:35 +0000

    add an7581-xg-140g-y003 & refresh an7581-xg-040g-md

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -52,14 +52,29 @@ define Device/bell_xg-040g-md
   KERNEL_LOADADDR := 0x80088000
   BLOCKSIZE := 128k
   PAGESIZE := 2048
-  KERNEL_SIZE := 10240k
+  KERNEL_SIZE := 8704k
   IMAGE_SIZE := 261120k
   KERNEL_IN_UBI := 1
   UBINIZE_OPTS := -E 5 -m 2048 -p 128KiB -s 2048
-  DEVICE_PACKAGES := airoha-en7581-npu-firmware kmod-phy-airoha-en8811h kmod-i2c-an7581 kmod-input-gpio-keys-polled
+  DEVICE_PACKAGES := airoha-en7581-npu-firmware kmod-phy-airoha-en8811h kmod-i2c-an7581 kmod-input-gpio-keys-polled uboot-envtools
   IMAGES += factory.bin sysupgrade.bin
-  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi | check-size
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
   SOC := an7581
 endef
 TARGET_DEVICES += bell_xg-040g-md
+
+define Device/bell_xg-140g-y003
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Nokia Bell
+  DEVICE_MODEL := XG-140G-Y003
+  DEVICE_DTS := an7581-xg-140g-y003
+  KERNEL_SIZE := 8704k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := airoha-en7581-npu-firmware kmod-phy-airoha-en8811h kmod-gpio-button-hotplug kmod-i2c-an7581 uboot-envtools 
+endef
+TARGET_DEVICES += bell_xg-140g-y003
