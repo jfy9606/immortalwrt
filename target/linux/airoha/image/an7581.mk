d917a872678d71241339041dd1f6594cacacd54e
jfy <jfy9606@qq.com>
2026-02-12 01:10:23 +0000

    add airoha-bell-xg-140g-y003

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -62,3 +62,24 @@ define Device/bell_xg-040g-md
   SOC := an7581
 endef
 TARGET_DEVICES += bell_xg-040g-md
+
+define Device/bell_xg-140g-y003
+  $(call Device/FitImageLzma)
+  DEVICE_VENDOR := Bell
+  DEVICE_MODEL := Bell XG-140G-Y003
+  DEVICE_DTS := an7581-xg-140g-y003
+  KERNEL_SIZE := 8704k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+  DEVICE_PACKAGES := \
+	kmod-phy-airoha-en8811h \
+	kmod-gpio-button-hotplug \
+	kmod-usb3 \
+	kmod-usb-xhci-mtk \
+	kmod-i2c-an7581 \
+	uboot-envtools
+endef
+TARGET_DEVICES += bell_xg-140g-y003
